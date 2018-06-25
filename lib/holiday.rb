7eeb189c56ec91b1106fa4cc18cc8969ce0a986b
Jesse McReady <jessemcready@gmail.com>
2018-06-25 10:13:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.to_s.capitalize!}:"
     data.each do |holiday, supplies|
-      holiday_array = holiday.to_s.split("_")
+      holiday_array = holiday.to_s.split
       holiday_array.map do |holiday|
         holiday.capitalize!
       end
